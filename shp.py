bdf4c3b68e027a69ba2388f2be24ca1bc55fab4d
fallou <130919182+Falloudiagnessm@users.noreply.github.com>
2023-11-25 13:59:57 +0000

    Update shp.py

--- shp.py
+++ shp.py
@@ -10,28 +10,40 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 def load_shapefile(shapefile_path):
     return gpd.read_file(shapefile_path)
 
-shapefile_path = "gadm36_SEN_1.shx"
-gdf = load_shapefile(shapefile_path)
-
+shapefile_path1 = "gadm36_SEN_1.shx"
+gdf1 = load_shapefile(shapefile_path1)
+shapefile_path2 = "praguefile_5.shp"
+gdf2 = load_shapefile(shapefile_path2)
 # Liste des arrondissements uniques dans le jeu de données de Thies
 #thies_arrondissements = gdf[gdf['NAME_1'] == 'Thiès']['NAME_3'].unique()
-st.write(gdf) 
-
+#st.write(gdf1) 
+st.write(gdf2) 
 # Création de la carte avec GeoPandas
 st.title("Carte  Senegal")
 fig, ax = plt.subplots()
-gdf.plot(ax=ax, color='gray')  # Rendre les autres arrondissements en gris pr défaut
+gdf1.plot(ax=ax, color='gray')  # Rendre les autres arrondissements en gris pr défaut
 
 fig1, ax = plt.subplots()
 
-gdf[gdf['NAME_1'] == 'Dakar'].plot(ax=ax, color='gray') 
+gdf1[gdf1['NAME_1'] == 'Thies'].plot(ax=ax, color='gray') 
 
 fig2, ax = plt.subplots()
 
-gdf[gdf['NAME_1'] == 'Diourbel'].plot(ax=ax, color='gray') 
+gdf1[gdf1['NAME_1'] == 'Diourbel'].plot(ax=ax, color='gray') 
  
 st.pyplot(fig)
-st.title("Carte  dakar")
+st.title("Carte  De la region de Thies")
 st.pyplot(fig1)
-st.title("Carte  Diourbel")
-st.pyplot(fig2)
+#st.title("Carte  Diourbel")
+#st.pyplot(fig2)
+
+#demarche
+#git clone https://github.com/Falloudiagnessm/shp.git
+#cd shp
+#git commit -m "moncommit"
+#git add .
+#git push -u origin main
+#mot de pass
+
+#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
+#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
